0f2bf93692e37a185e49e793b544191ac4e0a6a3
sarathchandra200795-beep <sarathchandra200795@gmail.com>
2025-12-18 01:41:07 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'loto-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
